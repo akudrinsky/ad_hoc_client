db9386f387985729f69088dbf75b6409fe3633da
Aleksei Kudrinskii <kudrinsky@MBP.local>
2021-03-09 11:24:29 +0300

    Initial commit, ChatListWidget does not work

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:ad_hoc_client/widgets/ChatListWidget.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChatListWidget();
+  }
+}
